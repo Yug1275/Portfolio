e1272ab2711c32ed6e0c8ea9a4b85d769d059a22
Yug1275 <yjpatel1275@gmail.com>
2025-07-20 17:31:24 +0530

    Added script.js for multiple-text

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Fronted Developer","AI-ML Enthusiasts","Merit-cum-scholar"],
+    typeSpeed:30,
+    backSpeed:30,
+    backDelay:1000,
+    loop: true
+})
